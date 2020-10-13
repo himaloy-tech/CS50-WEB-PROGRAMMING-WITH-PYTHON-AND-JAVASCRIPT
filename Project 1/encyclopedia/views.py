f2d4d0e52a6294b6f3016391f2b45cf9f8c8d15d
Himaloy <himaloymondal100@gmail.com>
2020-10-13 08:56:07 +0530

    Modified

--- Project 1/encyclopedia/views.py
+++ Project 1/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
-
+import markdown2
 from . import util
 
 
@@ -8,32 +8,30 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def details(request, title):
+    """
+    detail of an entry
+    """
     detail = util.get_entry(title=title)
-    normal = detail.replace(f'# {title}', '')
-    if detail != None:
-        return render(request, "encyclopedia/details.html", {
-            "title":title,
-            "text":normal.split(),
-        })
-    else:
-        return HttpResponse("Page not found")
+    normal_text = markdown2.markdown(detail)
+    return render(request, 'encyclopedia/details.html', {
+        "text":normal_text,
+        "title":title
+    })
 
 def search(request):
-    if request.method == "GET":
-        query = request.GET.get('q')
-        result = util.get_entry(title=query)
-        if result is not None:
-            return redirect(f'/details/{query}')
-        else:
-            results = []
-            for entry in util.list_entries():
-                if query in entry:
-                    results.append(entry)
-                else:
-                    continue
-            return render(request, 'encyclopedia/results.html', context={
-                "results":results
-            })
+    query = request.GET.get('q')
+    result = util.get_entry(title=query)
+    if result is not None:
+        return redirect(f'/details/{query}')
     else:
-        return HttpResponse("Page not found")
+        results = []
+        for entry in util.list_entries():
+            if query in entry:
+                results.append(entry)
+            else:
+                continue
+        return render(request, 'encyclopedia/results.html', {
+            "results":results
+        })
