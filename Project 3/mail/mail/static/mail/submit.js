4eaea5d80a733dbb655d3d439e178241e63db24c
Himaloy Mondal <himaloymondal100@gmail.com>
2021-01-22 15:54:59 +0530

    Added Send email feature

---
+++ Project 3/mail/mail/static/mail/submit.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const submit_button = document.querySelector('#submit_button');
+    submit_button.addEventListener('click', function (event){
+        const recipeints = document.querySelector('#compose-recipients').value;
+        const subject = document.querySelector('#compose-subject').value;
+        const body = document.querySelector('#compose-body').value;
+        fetch('/emails', {
+            method: 'POST',
+            body: JSON.stringify({
+                recipients: recipeints,
+                subject: subject,
+                body: body
+            })
+          })
+          .then(response => response.json())
+          .then(result => {
+            if(result.status == 400){
+                message.style.display = 'block';
+                message.innerHTML = result;
+            } else{
+                load_mailbox('sent');
+            }
+        });
+    });
+});
