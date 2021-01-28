8530a24b94c614e0ef059f7bc5423be61d93187b
Himaloy Mondal <himaloymondal100@gmail.com>
2021-01-28 12:25:05 +0530

    Added Send email Feature

--- Project 3/mail/mail/static/mail/inbox.js
+++ Project 3/mail/mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -15,8 +15,6 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('form').style.display = 'block';
-  const message = document.querySelector('#message');
-  message.style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -25,11 +23,16 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('form').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+      
+    })
 }
--- Project 3/mail/mail/static/mail/submit.js
+++ Project 3/mail/mail/static/mail/submit.js
@@ -1,13 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const submit_button = document.querySelector('#submit_button');
-    submit_button.addEventListener('click', function (event) {
-        const recipeints = document.querySelector('#compose-recipients').value;
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.onsubmit = function (event) {
+        event.preventDefault();
+        const recipients = document.querySelector('#compose-recipients').value;
         const subject = document.querySelector('#compose-subject').value;
         const body = document.querySelector('#compose-body').value;
+        if (from.length == 0 && recipients.length == 0) return;
         fetch('/emails', {
             method: 'POST',
             body: JSON.stringify({
-                recipients: recipeints,
+                recipients: recipients,
                 subject: subject,
                 body: body
             })
@@ -15,12 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.json())
         .then(result => {
             console.log(result);
-            if (result.status == 400) {
-                message.style.display = 'block';
-                message.innerHTML = result;
-            } else {
-                load_mailbox('sent');
-            }
+            load_mailbox('sent');
         });
-    });
+    }
 });
