b9c5299e16093fe1d708955070dbea2f8a60add9
Himaloy Mondal <himaloymondal100@gmail.com>
2021-04-14 10:32:59 +0530

    Added Create Course Feature

--- Project 5/capstone/eduHub/models.py
+++ Project 5/capstone/eduHub/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+    desc = models.TextField()
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+class Course(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to='thumbnails')
+    desc = models.TextField()
+    title = models.TextField(default="")
+    category = models.TextField(default="")
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="posts")
+    def __str__(self) -> str:
+        return f"{self.title}"
--- Project 5/capstone/eduHub/views.py
+++ Project 5/capstone/eduHub/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Course
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    course = Course.objects.all()
+    print(course)
+    return render(request, "index.html", {
+        "Course":Course.objects.all()
+    })
